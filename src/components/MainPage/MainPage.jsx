9593263d2fdc693708d2a4cc58cd665d23ba9221
happypotato187 <103550844+happypotato187@users.noreply.github.com>
2022-07-09 10:30:34 +0700

    рр

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,7 +1,15 @@
-
+import s from './MainPage.module.css'
 
 export const MainPage = (props) => {
 	return (
-		<div>MainPage</div>
+		<><div></div><Statistic /></>
+	)
+}
+
+const Statistic = () => {
+	return (
+		<div className = {s.statisticBlock}> 
+		
+		</div>
 	)
 }
