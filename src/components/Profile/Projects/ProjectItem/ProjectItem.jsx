cfc061198931a0f1b05355eeaa684547e0a40601
Максим <maksim_bulavin@mail.ru>
2022-07-10 19:09:16 +0700

    add common

--- src/components/Profile/Projects/ProjectItem/ProjectItem.jsx
+++ src/components/Profile/Projects/ProjectItem/ProjectItem.jsx
@@ -5,7 +5,7 @@ export const ProjectItem = (props) => {
 	return (
 		<div className={s.projectEl}>
 			<div className={s.projectEl_title}>{props.projectName}</div>
-			<div className={s.projectEl_title}>{props.creatorName}</div>
+			<div >{props.creatorName}</div>
 			<div className={s.projectEl_desk}>{props.projectDesc}</div>
 			<div className={s.summ_wrapper}>
 				<div className={s.lineEmpty}><div className={s.lineFilled}></div></div>
